d194e3eca7b85cfcf67bbe95d984742d960b8e91
ilyvion <alexschrod@gmail.com>
2025-01-23 09:52:20 +0000

    Deploying to gh-pages from @ ilyvion/bevy_image_font@19eaeaf1e20c5fdf48905490285cf9a72c278493 🚀

---
+++ trait.impl/bevy_reflect/enums/enum_trait/trait.Enum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["bevy_image_font",[["impl Enum for <a class=\"enum\" href=\"bevy_image_font/atlas_sprites/enum.ScalingMode.html\" title=\"enum bevy_image_font::atlas_sprites::ScalingMode\">ScalingMode</a><div class=\"where\">where\n    <a class=\"enum\" href=\"bevy_image_font/atlas_sprites/enum.ScalingMode.html\" title=\"enum bevy_image_font::atlas_sprites::ScalingMode\">ScalingMode</a>: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/any/trait.Any.html\" title=\"trait core::any::Any\">Any</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>,</div>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[783]}
