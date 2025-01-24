66af08decfd82c9013b1ba3f73ab1230b06d2b0f
ilyvion <alexschrod@gmail.com>
2025-01-24 08:24:38 +0000

    Deploying to gh-pages from @ ilyvion/bevy_image_font@c4f6cfa8993c14f2ed13666425c0d19131e61a20 🚀

--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bevy_image_font",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"bevy_image_font/struct.ImageFontSet.html\" title=\"struct bevy_image_font::ImageFontSet\">ImageFontSet</a>"]]]]);
+    var implementors = Object.fromEntries([["bevy_image_font",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"bevy_image_font/atlas_sprites/enum.ScalingMode.html\" title=\"enum bevy_image_font::atlas_sprites::ScalingMode\">ScalingMode</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"bevy_image_font/struct.ImageFontSet.html\" title=\"struct bevy_image_font::ImageFontSet\">ImageFontSet</a>"]]],["bevy_image_font",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"bevy_image_font/struct.ImageFontSet.html\" title=\"struct bevy_image_font::ImageFontSet\">ImageFontSet</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[302]}
+//{"start":57,"fragment_lengths":[603,303]}
